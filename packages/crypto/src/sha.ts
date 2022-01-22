dead434a5d84464bf03dd630043ed459886faaa9
Simon Warta <simon@warta.it>
2022-01-22 14:01:15 +0100

    Use Sha2 from @noble/hashes

--- packages/crypto/src/sha.ts
+++ packages/crypto/src/sha.ts
@@ -1,44 +1,14 @@
-import { Hash } from "crypto";
-import shajs from "sha.js";
+import { sha256 as nobleSha256 } from "@noble/hashes/sha256";
+import { sha512 as nobleSha512 } from "@noble/hashes/sha512";
 
 import { HashFunction } from "./hash";
-
-export class Sha1 implements HashFunction {
-  public readonly blockSize = 512 / 8;
-
-  private readonly impl: Hash;
-
-  public constructor(firstData?: Uint8Array) {
-    this.impl = shajs("sha1");
-
-    if (firstData) {
-      this.update(firstData);
-    }
-  }
-
-  public update(data: Uint8Array): Sha1 {
-    this.impl.update(data);
-    return this;
-  }
-
-  public digest(): Uint8Array {
-    return new Uint8Array(this.impl.digest());
-  }
-}
-
-/** Convenience function equivalent to `new Sha1(data).digest()` */
-export function sha1(data: Uint8Array): Uint8Array {
-  return new Sha1(data).digest();
-}
 
 export class Sha256 implements HashFunction {
   public readonly blockSize = 512 / 8;
 
-  private readonly impl: Hash;
+  private readonly impl = nobleSha256.create();
 
   public constructor(firstData?: Uint8Array) {
-    this.impl = shajs("sha256");
-
     if (firstData) {
       this.update(firstData);
     }
@@ -62,11 +32,9 @@ export function sha256(data: Uint8Array): Uint8Array {
 export class Sha512 implements HashFunction {
   public readonly blockSize = 1024 / 8;
 
-  private readonly impl: Hash;
+  private readonly impl = nobleSha512.create();
 
   public constructor(firstData?: Uint8Array) {
-    this.impl = shajs("sha512");
-
     if (firstData) {
       this.update(firstData);
     }
